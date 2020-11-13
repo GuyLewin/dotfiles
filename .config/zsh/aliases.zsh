6aca2b95b3ef1154ee7d1484a489948c3faf1a12
Guy Lewin <guy@lewin.co.il>
2020-11-13 10:36:02 -0500

    style: clearer alias names

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,5 +1,6 @@
-alias gitcleanbranches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
-alias dockerkillall='docker container kill $(docker ps -q)'
+alias git_clean_branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
+alias docker_kill_all='docker container kill $(docker ps -q)'
+alias docker_rm_all_containers='docker container rm $(docker container ls -aq)'
 
 # https://www.atlassian.com/git/tutorials/dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
