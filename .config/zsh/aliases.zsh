ca38aed5d1f18e120251639aa145965993201373
Guy Lewin <guy@lewin.co.il>
2022-09-09 10:51:10 -0400

    feat: add delete all images alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,6 +1,7 @@
 alias git_clean_branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 alias docker_kill_all='docker container kill $(docker ps -q)'
 alias docker_rm_all_containers='docker container rm $(docker container ls -aq)'
+alias docker_rm_all_images='docker image rm $(docker image ls -aq)'
 
 # https://www.atlassian.com/git/tutorials/dotfiles
 alias config='/opt/homebrew/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
